ceec01d9a01aaf8aa5d9fb8d862c4ed4b9e946aa
Hassanjadi <hassanjadi25@gmail.com>
2024-11-08 14:56:06 +0700

    feat: request from api

---
+++ src/scripts/data/therestodb-source.js
@@ -0,0 +1,16 @@
+import API_ENDPOINT from "../globals/api-endpoint";
+
+class TheRestaurantDbSource {
+  static async restaurantList() {
+    const response = await fetch(API_ENDPOINT.RESTO_LIST);
+    const responseJson = await response.json();
+    return responseJson;
+  }
+
+  static async detailRestaurant(id) {
+    const response = await fetch(API_ENDPOINT.RESTO_DETAIL(id));
+    return response.json();
+  }
+}
+
+export default TheRestaurantDbSource;
