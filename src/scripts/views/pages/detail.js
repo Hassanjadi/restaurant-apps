47dc756488b9e707e5051c5b57737bafdc100575
Hassan Jadi <hassanjadi25@gmail.com>
2024-12-25 08:02:04 +0700

    feat: favorite restaurant

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import UrlParser from "../../routes/url-parser";
 import TheRestaurantDbSource from "../../data/therestodb-source";
-import { createRestaurantDetailTemplate } from "../../views/templates/templates-creator";
+import { createRestaurantDetailTemplate } from '../templates/templates-creator';
+import LikeButtonInitiator from "../../utils/like-button-initiator";
 
 const Detail = {
   async render() {
     return `
       <div id="restaurant" class="restaurant"></div>
+      <div id="likeButtonContainer"></div>
     `;
   },
 
@@ -15,6 +17,18 @@ const Detail = {
     const restaurant = response.restaurant;
     const restaurantContainer = document.querySelector("#restaurant");
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: restaurant.id,
+        pictureId: restaurant.pictureId,
+        name: restaurant.name,
+        description: restaurant.description,
+        rating: restaurant.rating,
+      },
+    });
   },
 };
 
--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,13 +1,26 @@
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+import { createRestaurantItemTemplate } from '../templates/templates-creator';
+
 const Favorite = {
   async render() {
     return `
-      <h2>Favorite Page</h2>
-    `;
+    <div class="content">
+      <h2 class="content__heading">Your Favorite Restaurant</h2>
+      <div id="restaurant" class="restaurant">
+ 
+      </div>
+    </div>
+  `;
   },
-
+ 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const restaurant = await FavoriteRestaurantIdb.getAllRestaurant();
+    const restaurantContainer = document.querySelector('#restaurant');
+    
+    restaurant.forEach((restaurant) => {
+      restaurantContainer.innerHTML += createRestaurantItemTemplate(restaurant);
+    });
   },
 };
-
+ 
 export default Favorite;
