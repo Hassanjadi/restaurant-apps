9988e97a2d4f11a25d824285883013dae030ffcb
Hassan Jadi <hassanjadi25@gmail.com>
2025-01-15 06:36:40 +0700

    feat: codecept e2e testing

---
+++ e2e/Liking_Restaurant.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+Feature('Liking Restaurant');
+
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked restaurant',  ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.restaurant-item_not_found');
+});
