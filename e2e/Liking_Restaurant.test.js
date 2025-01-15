11c3bb4cc4cbbcbe5633d2bfdfb26cca6b49f270
Hassan Jadi <hassanjadi25@gmail.com>
2025-01-16 06:43:25 +0700

    feat: e2e testing

--- e2e/Liking_Restaurant.test.js
+++ e2e/Liking_Restaurant.test.js
@@ -1,11 +1,32 @@
-/* eslint-disable no-undef */
-Feature('Liking Restaurant');
+// eslint-disable-next-line no-unused-vars, no-undef
+const { I } = inject();
 
+// eslint-disable-next-line no-undef
+Feature('Restaurant Favorites');
+
+// eslint-disable-next-line no-undef
 Before(({ I }) => {
-  I.amOnPage('/#/like');
+  I.amOnPage('/');
 });
 
-Scenario('showing empty liked restaurant',  ({ I }) => {
-  I.seeElement('#query');
-  I.see('Tidak ada film untuk ditampilkan', '.restaurant-item_not_found');
+// eslint-disable-next-line no-undef
+Scenario('Liking a restaurant', async ({ I }) => {
+  I.waitForElement('.resto-item_description', 5);
+  I.seeElement('.resto-item_description');
+  I.click('.resto-item > a ');
+
+  I.waitForElement('#likeButton', 2);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+});
+
+// eslint-disable-next-line no-undef
+Scenario('Unliking a restaurant', async ({ I }) => {
+  I.waitForElement('.resto-item_description', 5);
+  I.seeElement('.resto-item_description');
+  I.click('.resto-item > a ');
+
+  I.waitForElement('#likeButton', 2);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 });
